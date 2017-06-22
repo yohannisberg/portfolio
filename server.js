7f14fa14659cacda3aa457e34d9d264c50e30e90
John D'Agostino <jdagos3@gmail.com>
2017-06-21 21:59:14 -0700

    added server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express'),
+    app = module.exports = express(),
+    config = require('./config.js'),
+    bodyParser = require('body-parser'),
+    session = require('express-session');
+
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/dist'));
+
+app.listen(config.port, () => {
+    console.log(`listening on port ${config.port}`)
+})
