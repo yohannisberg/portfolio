ee9c5064df7d2b1db7da59e60dd86701612f7078
John D'Agostino <jdagos3@gmail.com>
2017-07-19 21:12:03 -0700

    added Purely By Chance project

--- js/agency.js
+++ js/agency.js
@@ -1,5 +1,4 @@
 // Agency Theme JavaScript
-
 (function($) {
     "use strict"; // Start of use strict
 
@@ -19,7 +18,7 @@
     });
 
     // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a').click(function(){ 
+    $('.navbar-collapse ul li a').click(function(){
             $('.navbar-toggle:visible').click();
     });
 
